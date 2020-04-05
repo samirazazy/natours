85c9425f8e94eebf7f590b158d619246954105e6
samirazazy <samiralazazy@gmail.com>
2020-04-05 20:11:39 +0200

    import dec  data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    // https://stackoverflow.com/questions/57895175/server-discovery-and-monitoring-engine-is-deprecated
+    useUnifiedTopology: true
+  })
+  .then(con => {
+    // console.log(con.connection);
+    console.log('DB connection successful');
+  });
+
+// Read json file
+const tours = fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf8');
+
+// Import dsts inti database
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully imported');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// delete all data from the database
+const deleteData = async () => {
+  try {
+    await tours.deleteMany();
+    console.log('data deleted successfully');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+console.log(process.argv);
