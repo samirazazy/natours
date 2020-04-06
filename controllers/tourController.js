9787d76f062e7b5e4a70e6ed1e50eb96ac8489d5
samirazazy <samiralazazy@gmail.com>
2020-04-06 14:36:02 +0200

    aad get all tours quiry

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,9 +2,21 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+    const query = Tour.find(queryObj);
+
+    // const query = Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    const tours = await query;
     res.status(200).json({
       status: 'success',
+      results: tours.length,
       data: {
         tours
       }
