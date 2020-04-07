a5a286d7fd8cb869db2d5abefa7c4813aec0ab02
samirazazy <samiralazazy@gmail.com>
2020-04-07 16:28:21 +0200

    aggregiation pipline

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,6 +9,8 @@ router
   .route('/top-5-cheap')
   .get(tourController.aslisTopTours, tourController.getAllTours);
 
+router.route('/tours-stats').get(tourController.getTourStats);
+
 router
   .route('/')
   .get(tourController.getAllTours)
