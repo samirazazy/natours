e2f5ad1f9dbc162e9ea6eabfb467702494e2e6e7
samirazazy <samiralazazy@gmail.com>
2020-04-08 15:01:25 +0200

    add globalErrorHanderer

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHanderer = require('./controllers/errorControler');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -27,5 +29,20 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Mudt be in the end of routers
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Cannot find ${req.originalUrl} on the server`
+  // });
+  // const err = new Error(`Cannot find ${req.originalUrl} on the server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  next(new AppError(`Cannot find ${req.originalUrl} on the server`, 404));
+});
+
+app.use(globalErrorHanderer);
 
 module.exports = app;
