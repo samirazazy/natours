df03a322b90fc89a30abdba8ea963587a88685cb
samirazazy <samiralazazy@gmail.com>
2020-04-14 20:19:59 +0200

    13. Nested Routes with Express

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
+
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createTour
+//   );
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
