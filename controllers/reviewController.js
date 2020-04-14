854c9bd046c281284d3ee8a8ba525cf64ab12f27
samirazazy <samiralazazy@gmail.com>
2020-04-14 20:30:20 +0200

    Get all reviews on tour

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,7 +2,9 @@ const Review = require('./../models/reviewModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const review = await Review.find();
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+  const review = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
