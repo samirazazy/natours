846901e8feeaa0cfa3ac8f29857ed26106293f4b
samirazazy <samiralazazy@gmail.com>
2020-04-16 14:28:04 +0200

    Adding Missing Authentication and Authorization

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -10,8 +10,8 @@ router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
+    reviewController.setTourUserIds,
     reviewController.createReview
   );
 
