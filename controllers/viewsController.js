3e2f95732e11662a18ce32ce4a4e35b8198425bc
samirazazy <samiralazazy@gmail.com>
2020-04-18 20:25:48 +0200

    Including a Map with Mapbox

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -34,33 +34,33 @@ exports.getTour = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getLoginForm = (req, res) => {
-  res.status(200).render('login', {
-    title: 'Log into your account'
-  });
-};
+// exports.getLoginForm = (req, res) => {
+//   res.status(200).render('login', {
+//     title: 'Log into your account'
+//   });
+// };
 
-exports.getAccount = (req, res) => {
-  res.status(200).render('account', {
-    title: 'Your account'
-  });
-};
+// exports.getAccount = (req, res) => {
+//   res.status(200).render('account', {
+//     title: 'Your account'
+//   });
+// };
 
-exports.updateUserData = catchAsync(async (req, res, next) => {
-  const updatedUser = await User.findByIdAndUpdate(
-    req.user.id,
-    {
-      name: req.body.name,
-      email: req.body.email
-    },
-    {
-      new: true,
-      runValidators: true
-    }
-  );
+// exports.updateUserData = catchAsync(async (req, res, next) => {
+//   const updatedUser = await User.findByIdAndUpdate(
+//     req.user.id,
+//     {
+//       name: req.body.name,
+//       email: req.body.email
+//     },
+//     {
+//       new: true,
+//       runValidators: true
+//     }
+//   );
 
-  res.status(200).render('account', {
-    title: 'Your account',
-    user: updatedUser
-  });
-});
+//   res.status(200).render('account', {
+//     title: 'Your account',
+//     user: updatedUser
+//   });
+// });
