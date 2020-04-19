6c3ece1a564b109f90c5b86e2091764630c5bc2b
samirazazy <samiralazazy@gmail.com>
2020-04-19 20:31:54 +0200

    instal sharp package

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+const sharp = require('sharp');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
