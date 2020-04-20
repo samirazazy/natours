490143d6de45c69d8f85fa4f888989eeb4b855c0
samirazazy <samiralazazy@gmail.com>
2020-04-20 19:30:58 +0200

    Creating New Bookings on Checkout Success

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,6 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
-// const Booking = require('../models/bookingModel');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 // const factory = require('./handlerFactory');
 
@@ -10,9 +10,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   console.log(tour);
 
   // 2) Create checkout session
+  // http://127.0.0.1:3000/?tour=5c88fa8cf4afda39709c2970&user=5c8a1f292f8fb814b56fa184&price=2997
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -22,15 +25,26 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         description: tour.summary,
         images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
         amount: tour.price * 100,
-        // currency: 'egp',
         currency: 'usd',
         quantity: 1
       }
     ]
   });
+
   // 3) Create session as response
   res.status(200).json({
     status: 'success',
     session
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
+  console.log(req.originalUrl);
 });
